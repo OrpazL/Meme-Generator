c5991708d17d2bcf45dd2bb6e53073699fa3e339
ilan90 <ilanrot90@gmail.com>
2018-10-25 10:46:33 +0300

    commit

---
+++ js/search.js
@@ -0,0 +1,17 @@
+function startSearch(e) {
+    e.preventDefault();
+
+    const regex = new RegExp(escapeRegex($('.search').val()), 'gi');
+
+    let images = findImages(regex);
+    console.log(options);
+    createList(images);
+}
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+function findImages(regex) {
+    return gImages.filter(img => img.keywords.find(keyword => regex.test(keyword)) !== undefined );
+}
